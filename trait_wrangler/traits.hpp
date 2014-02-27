ebe88c50e496dfa07462b8e20c24ff29ebf2545e
Coranna Howard <me@komiga.com>
2014-02-27 14:18:53 -0500

    Corrected copyright holder name.¹
    1: It's your legal name, doofus.

--- trait_wrangler/traits.hpp
+++ trait_wrangler/traits.hpp
@@ -2,8 +2,8 @@
 @file traits.hpp
 @brief Non-standard traits and trait operators.
 
-@author Tim Howard
-@copyright 2013-2014 Tim Howard under the MIT license;
+@author Timothy Howard
+@copyright 2013-2014 Timothy Howard under the MIT license;
 see @ref index or the accompanying LICENSE file for full text.
 */
 
